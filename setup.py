701c69e52bb551e180ac4304c33dce4134e87a8a
Mike Gray <mike@graywind.org>
2023-05-02 21:46:06 -0500

    chore: proper packaging

--- setup.py
+++ setup.py
@@ -24,30 +24,26 @@ from setuptools import setup
 from os import getenv, path, walk
 
 SKILL_NAME = "skill-plex"
-SKILL_PKG = SKILL_NAME.replace('-', '_')
+SKILL_PKG = SKILL_NAME.replace("-", "_")
 # skill_id=package_name:SkillClass
-PLUGIN_ENTRY_POINT = f'{SKILL_NAME}.d-mcknight={SKILL_PKG}:PlexSkill'
+PLUGIN_ENTRY_POINT = f"{SKILL_NAME}.d-mcknight={SKILL_PKG}:PlexSkill"
 BASE_PATH = path.abspath(path.dirname(__file__))
 
 
 def get_requirements(requirements_filename: str):
     requirements_file = path.join(BASE_PATH, requirements_filename)
-    with open(requirements_file, 'r', encoding='utf-8') as r:
+    with open(requirements_file, "r", encoding="utf-8") as r:
         requirements = r.readlines()
-    requirements = [r.strip() for r in requirements if r.strip()
-                    and not r.strip().startswith("#")]
+    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
 
     for i in range(0, len(requirements)):
         r = requirements[i]
         if "@" in r:
-            parts = [p.lower() if p.strip().startswith("git+http") else p
-                     for p in r.split('@')]
+            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split("@")]
             r = "@".join(parts)
         if getenv("GITHUB_TOKEN"):
             if "github.com" in r:
-                requirements[i] = \
-                    r.replace("github.com",
-                              f"{getenv('GITHUB_TOKEN')}@github.com")
+                requirements[i] = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
     return requirements
 
 
@@ -57,12 +53,10 @@ def find_resource_files():
     package_data = ["skill.json"]
     for res in resource_base_dirs:
         if path.isdir(path.join(base_dir, res)):
-            for (directory, _, files) in walk(path.join(base_dir, res)):
+            for directory, _, files in walk(path.join(base_dir, res)):
                 if files:
-                    package_data.append(
-                        path.join(directory.replace(base_dir, "").lstrip('/'),
-                                  '*'))
-#    print(package_data)
+                    package_data.append(path.join(directory.replace(base_dir, "").lstrip("/"), "*"))
+    #    print(package_data)
     return package_data
 
 
@@ -80,16 +74,16 @@ with open(path.join(BASE_PATH, "version.py"), "r", encoding="utf-8") as v:
 setup(
     name=f"{SKILL_NAME}",
     version=version,
-    url=f'https://github.com/d-mcknight/{SKILL_NAME}',
-    license='BSD-3-Clause',
+    url=f"https://github.com/d-mcknight/{SKILL_NAME}",
+    license="BSD-3-Clause",
     install_requires=get_requirements("requirements.txt"),
-    author='Daniel McKnight and Mike Gray',
-    author_email='daniel@neon.ai',
+    author="Daniel McKnight and Mike Gray",
+    author_email="daniel@neon.ai",
     long_description=long_description,
     long_description_content_type="text/markdown",
     package_dir={SKILL_PKG: ""},
-    # packages=[SKILL_PKG, f"{SKILL_PKG}.util"],
+    packages=[SKILL_PKG],
     package_data={SKILL_PKG: find_resource_files()},
     include_package_data=True,
-    entry_points={"ovos.plugin.skill": PLUGIN_ENTRY_POINT}
+    entry_points={"ovos.plugin.skill": PLUGIN_ENTRY_POINT},
 )
